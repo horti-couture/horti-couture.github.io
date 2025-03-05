3cc8d9082bf3f430ad79b27ddb5501546ab7be89
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-03-05 08:22:42 +0200

    Updates

--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Mike\Music\Horti-Couture-Website\Horti-Couture-Website\frontend\Horti-Couture-Website"
-npm start
-pause
--- update git.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\Mike\Music\Horti-Couture-Website\Horti-Couture-Website\frontend\Horti-Couture-Website"
-git add .
-git commit -m "Deploy website to luxcare.github.io"
-git push origin main
-npm run deploy
-pause
