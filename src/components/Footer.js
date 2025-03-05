2478f24d72784289b41be9c31a781893acaaed86
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-03-05 08:21:38 +0200

    Deploy website to luxcare.github.io

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import "../styles/Footer.css";
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>© {new Date().getFullYear()} LuxCare Cleaning Services. All Rights Reserved.</p>
+            <p>© {new Date().getFullYear()} Horti Couture. All Rights Reserved.</p>
             <p>
-                Contact: <a href="tel:0661290984" className="footer-link">066 129 0984</a> |  
-                Email: <a href="mailto:luxcarecleaning@gmail.com" className="footer-link">luxcarecleaning@gmail.com</a>
+                Contact: <a href="tel:072 356 7903" className="footer-link">072 356 7903</a> |  
+                Email: <a href="mailto:horticouture@eastcoastsa.net" className="footer-link">horticouture@eastcoastsa.net</a>
             </p>
         </footer>
     );
