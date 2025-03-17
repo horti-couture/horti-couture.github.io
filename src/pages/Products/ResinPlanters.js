87c5cd1749601001d8943bfea80ebf48724cd852
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-03-17 08:51:50 +0200

    Deploy backend https://backend-7dm6.onrender.com

--- src/pages/Products/ResinPlanters.js
+++ src/pages/Products/ResinPlanters.js
@@ -1,6 +1,7 @@
 // src/pages/Products/ResinPlanters.js
 import React from 'react';
 import ProductCard from '../../components/ProductCard/ProductCard';
+import PhotoViewer from '../../components/PhotoViewer/PhotoViewer';
 import products from '../../data/products';
 import './ResinPlanters.css';
 
@@ -15,6 +16,7 @@ const ResinPlanters = () => {
                     <ProductCard key={product.id} product={product} />
                 ))}
             </div>
+            <PhotoViewer /> {/* Add the Photo Viewer here */}
         </div>
     );
 };
