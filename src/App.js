69fc315bd217b4f80be5997ed19de26ec38e2aa2
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-04-10 14:40:35 +0200

    Deploy backend https://backend-7dm6.onrender.com

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Bookings from "./pages/Bookings";
